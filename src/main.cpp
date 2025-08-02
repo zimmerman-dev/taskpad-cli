696bed418a98ca7537ce1d238618c63b43ad945d
John Zimmerman <140193884+zimmerman-dev@users.noreply.github.com>
2025-08-02 14:11:07 -0500

    test: Test UI aesthetic

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,14 @@
 
 int main() {
 
+    std::cout << "\n";
+    std::cout << "==========================\n";
     std::cout << "TASKPAD-CLI (Session Mode)\n";
-
+    std::cout << "==========================\n";
+    std::cout << "1. Add Task\n";
+    std::cout << "2. View Session Tasks\n";
+    std::cout << "3. View Session Summary\n";
+    std::cout << "4. Quit Session\n";
+    std::cout << "\nEnter choice: _ ";
     return 0;
 }
